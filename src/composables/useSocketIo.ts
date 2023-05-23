b4ad0de94f3e28c9e608d8ce7de889aa15e6b4e2
Li-Ninja <lininja.tw@gmail.com>
2023-05-23 11:59:31 +0800

    feat: extraHeaders add token

--- src/composables/useSocketIo.ts
+++ src/composables/useSocketIo.ts
@@ -1,10 +1,18 @@
 import { io, Socket } from 'socket.io-client';
+import { useLocalStorage } from '@/utils/localStorage.util';
 
 let socketIo: Socket;
+const { getToken } = useLocalStorage();
 
 export function bootSocketIo(url: string) {
+  const token = getToken();
+
   if (!socketIo) {
-    socketIo = io(url);
+    socketIo = io(url, {
+      extraHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   }
 
   socketIo.on('connect', () => {
